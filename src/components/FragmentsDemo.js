54c67703ccbff98de8652cda1f4053a12942be7d
Anshika15 <bansalanshika20@gmail.com>
2021-03-05 19:59:55 +0530

    Fragments

---
+++ src/components/FragmentsDemo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FragmentsDemo() {
+    return(
+        <React.Fragment>
+            <h1>
+                Fragments Demo
+            </h1>
+            <p>Fragments</p>
+        </React.Fragment>
+    )
+}
+
+export default FragmentsDemo
